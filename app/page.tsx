5c9791305927e6fa3a3e8c3b1bf015c1da83c2ca
sopoturazashvili <sopoturazashvili@gmail.com>
2024-06-26 18:01:56 +0400

    deleted component import

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import AlbumCard from "./Components/AlbumCard/AlbumCard";
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        
+      
     </main>
   );
 }
