545a9ef033a87e9cd2eb8647223d0037b783a897
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-06-26 14:50:12 +0400

    CREATED COLOR AND ENUM

---
+++ app/enums/Colors.enums.ts
@@ -0,0 +1,13 @@
+export enum ColorEnum {
+    Primary_Black = '#0F0F0F',
+    Primary_Very_DarkBlack = "#121212",
+    Primary_DarkBlack = "#161616",
+    Primary_Darker_Black = "#1E1E1E",
+    Primary_EvenDarker_Black = "#232323" ,
+    Primary_BrightBlue = "#5E4BE2" ,
+    Primary_LightBlue = "#5141BF" ,
+    Primary_DarkBlue = "#2F3355" ,
+    Primary_Grey = "#AAA" ,
+    Primary_LightGrey = "#ABABAB" ,
+    Primary_White = "#FFF"
+}
