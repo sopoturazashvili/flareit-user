54acf2cb48bf1b20cbcaf2b5396b9b5853698bb4
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-06-27 18:26:00 +0400

    Created MusicCard Component

---
+++ app/Components/MusicCard/MusicCard.tsx
@@ -0,0 +1,35 @@
+import styles from "./MusicCard.module.scss"
+import LikeButton from "../LikeButton/LikeButton"
+
+
+
+interface Props {
+    image: string,
+    title: string,
+    teamName: string
+}
+
+const MusicCard = (props:Props) => {
+
+    return (
+        <div className={styles.musicCard}>
+            <div className={styles.musicCardHeader}>
+                <div className={styles.musicCardPhotoAndHoverCont}>
+                    <img className={styles.musicCardPhoto} src={props.image} />
+                    <div className={styles.musicCardHoverPhoto} >
+                        <img src="/Image/PlayHover.svg" />
+                    </div>
+                </div>
+                <div className={styles.musicCardTitle}>
+                    <span className={styles.musicCardName}>{props.title}</span>
+                    <span className={styles.musicCardTeam}>{props.teamName}</span>
+                </div>
+            </div>
+            <div>
+                <LikeButton isLiked={false} />
+            </div>
+        </div>
+    )
+}
+
+export default MusicCard 
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import styles from "./page.module.css";
 
 
 
+
+
 export default function Home() {
   return (
     <main className={styles.main}>
-        
+
     </main>
   );
 }
