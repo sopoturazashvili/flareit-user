90fecfc95e9bf30a7ea55f2dbafd8401186d470f
sopoturazashvili <sopoturazashvili@gmail.com>
2024-07-05 15:28:20 +0400

    refactor: reusable button

--- app/Components/Button/Button.tsx
+++ app/Components/Button/Button.tsx
@@ -5,20 +5,23 @@ interface Props {
     size?: 'large';
     text: string;
     width: string;
-    onClick?: () => void;
+    onClick: () => void;
 }
 
 const Button = (props: Props) => {
-
-    const buttonStyles = {
-        width: props.width,
-        ...(props.size == 'large' && {padding: '21px 205px', fontSize: '24px', height: '71px'})
-    }
-
+    const classNames = [
+        styles.btnCommon,
+        props.primary ? styles.primary : styles.secondary,
+        props.size === 'large' && styles.large
+    ]
+    .filter(Boolean)
+    .join(' ');
 
     return (
-        <button style={buttonStyles} onClick={props.onClick} className={`${styles.btnCommon} ${props.primary ? styles.primary : styles.secondary}`}>{props.text}</button>
+        <button style={{ width: props.width }} onClick={props.onClick} className={classNames}>
+            {props.text}
+        </button>
     )
 }
 
-export default Button
+export default Button;
