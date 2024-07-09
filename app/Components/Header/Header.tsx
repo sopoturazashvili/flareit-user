3682b7164f5225a3ae177e1ef9c529a37a0e39a3
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-07-09 20:21:05 +0400

    created header file

---
+++ app/Components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import styles from "./Header.module.scss"
+
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+        
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
