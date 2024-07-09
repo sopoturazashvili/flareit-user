0246637468584712744a27a0d5d4d521463c1c6c
sopoturazashvili <sopoturazashvili@gmail.com>
2024-07-09 14:09:40 +0400

    feat/created fill and text button

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import FillAndTextButton from "./Components/FillButton/FillAndTextButton";
 
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <FillAndTextButton text={"see all"} fill={false} />
     </main>
   );
 }
