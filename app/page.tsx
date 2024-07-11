ae98d69f4eb5727cd80184f4c66e43ec2c6e4951
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-07-11 15:51:51 +0400

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client"
+
 import Image from "next/image";
 import styles from "./page.module.scss";
-import HeaderAndNav from "./Components/Header/HeaderAndNavigation/HeaderAndNav";
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <HeaderAndNav/>
+
     </main>
   );
 }
