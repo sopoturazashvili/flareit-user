c0aa2c9b8a2faed067b6dab77d2234cb739e2fa4
sopoturazashvili <sopoturazashvili@gmail.com>
2024-07-15 16:11:02 +0400

    feat/wrapped component with Link tag

--- app/Components/ArtistPlaylistListItem/ArtistPlaylistItem.tsx
+++ app/Components/ArtistPlaylistListItem/ArtistPlaylistItem.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from './ArtistPlaylistItem.module.scss'
 import Image from 'next/image'
 
@@ -11,15 +12,17 @@ interface Props {
 
 const ArtistPlaylistItem = (props: Props) => {
     return (
-        <div className={styles.cardWrapper} onClick={props.onClick} >
-            <div className={styles.card}>
-                <div className={styles.cardContent}>
-                    <Image src={props.image} width={48} height={48} alt='Cover Image' className={props.imageRound ? styles.rounded : ''} />
-                    <h4 className={styles.text}>{props.text}</h4>
+        <Link href={''}>
+            <div className={styles.cardWrapper} onClick={props.onClick} >
+                <div className={styles.card}>
+                    <div className={styles.cardContent}>
+                        <Image src={props.image} width={48} height={48} alt='Cover Image' className={props.imageRound ? styles.rounded : ''} />
+                        <h4 className={styles.text}>{props.text}</h4>
+                    </div>
+                    <Image src={'/images/Arrow.svg'} width={24} height={24} alt='Go to page' />
                 </div>
-                <Image src={'/images/Arrow.svg'} width={24} height={24} alt='Go to page' />
             </div>
-        </div>
+        </Link>
     )
 }
 
