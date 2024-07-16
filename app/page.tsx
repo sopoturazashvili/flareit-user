244f7b187396538acdd53756639d5c367596be8a
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-07-16 19:17:32 +0400

    cursor pointer

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@
 
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Modal from "./Components/Modal/Modal";
+import PlayListInput from "./Components/Playlist/PlayListInput/PlayListInput";
+import CreatedPlaylist from "./Components/Playlist/CreatedPlaylist/CreatedPlaytlist";
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
-      
+      <CreatedPlaylist/>
     </main>
   );
 }
