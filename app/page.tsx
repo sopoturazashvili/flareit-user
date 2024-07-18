a0465c3d04bbe18352342be7d6b85326f86ec472
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-07-18 10:34:39 +0400

    delete page.tsx createdPlaylist

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@
 
 import Image from "next/image";
 import styles from "./page.module.scss";
-import CreatedPlaylist from "./Components/Playlist/CreatedPlaylist/CreatedPlaytlist";
 
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
-      <CreatedPlaylist />
+      
     </main>
   );
 }
