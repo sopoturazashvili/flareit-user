990fc44b77352ba818d169105f6771156b0de18a
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-07-19 16:27:29 +0400

    clean

--- app/(authorised)/layout.tsx
+++ app/(authorised)/layout.tsx
@@ -1,12 +1,6 @@
 import HeaderContainer from '../Components/Header/HeaderContainer';
 import NavDesktop from '../Components/Navigation/NavDesktop/NavDesktop';
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
 import NavMobile from '../Components/Navigation/NavMobile/NavMobile';
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 import Navigation from '../Components/Navigation/Navigation';
 import styles from './layout.module.scss'
 import { ReactNode } from 'react';
@@ -24,15 +18,9 @@ const Layout = (props: Props) => {
             <div className={styles.header}>
                 <HeaderContainer/>
                 {props.children}
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
                 <div className={styles.navMobile}>
                     <NavMobile/>
                 </div>
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
             </div>
         </div>
     )
