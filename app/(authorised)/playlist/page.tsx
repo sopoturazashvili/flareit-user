c908e416a6e141140f9a71ec0ead172868154221
sopoturazashvili <sopoturazashvili@gmail.com>
2024-07-19 09:54:40 +0400

    feat/playlist page

---
+++ app/(authorised)/playlist/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+import AddPlaylist from '@/app/Components/Playlist/AddPlaylist/AddPlaytlist'
+import styles from './page.module.scss'
+import ArtistPlaylistItem from '@/app/Components/ArtistPlaylistListItem/ArtistPlaylistItem'
+
+const Playlist = () => {
+    const playlists = [
+        {   
+            id: 1,
+            name: 'My Everyday',
+            image: '/images/myeveryday.png'
+        },
+        {   
+            id: 2,
+            name: 'Party Songs',
+            image: '/images/partySongs.png'
+        },
+        {   
+            id: 3,
+            name: 'Car Songs',
+            image: '/images/carSongs.png'
+        }
+    ]
+
+    return (
+        <div className={styles.container}>
+            <div>
+                <span className={styles.title}>Playlists</span>
+            </div>
+            <div className={styles.list}>
+                <AddPlaylist />
+                {playlists.map((playlist, index) => (
+                    <ArtistPlaylistItem image={playlist.image} text={playlist.name} imageRound={false} id={playlist.id} pagePathName={'playlist'} />
+                ) )}
+            </div>
+        </div>
+    )
+}
+
+export default Playlist
