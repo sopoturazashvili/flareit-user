3b836d21fad92cb02323356fe8a4c5c3ec31c586
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-07-23 12:26:03 +0400

    change mobile

--- app/(authorised)/page.tsx
+++ app/(authorised)/page.tsx
@@ -3,7 +3,5 @@
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <main className={styles.main}>
-   
-  </main>;
+  return <main className={styles.main}></main>;
 }
