673f71b36817537d9be79a8b54134009b4cc216b
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-07-24 18:15:14 +0400

    created popular artist

--- app/(authorised)/page.tsx
+++ app/(authorised)/page.tsx
@@ -1,7 +1,56 @@
 "use client";
 
+import ArtistCard from "../Components/ArtistCard/ArtistCard";
+import SectionHeader from "../Components/SectionHeader/SectionHeader";
+import TrendHitsCard from "../Components/TrendHitsCard/TrendHitsCard";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  const data = [
+    {
+      image: "/images/artistCard.svg",
+      artist: "coldplay",
+      year: "1997",
+      id: 0,
+    },
+    {
+      image: "/images/artistCard.svg",
+      artist: "coldplay",
+      year: "1997",
+      id: 1,
+    },
+    {
+      image: "/images/artistCard.svg",
+      artist: "coldplay",
+      year: "1997",
+      id: 2,
+    },
+    {
+      image: "/images/artistCard.svg",
+      artist: "coldplay",
+      year: "1997",
+      id: 3,
+    },
+  ];
+
+  return (
+    <main className={styles.main}>
+      <div>
+        <TrendHitsCard />
+      </div>
+      <div className={styles.sectionAndCard}>
+        <SectionHeader title={"see all"} href={"artists"} />
+        <div className={styles.artistCard}>
+          {data.map((item) => (
+            <ArtistCard
+              image={item.image}
+              artist={item.artist}
+              year={item.year}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
