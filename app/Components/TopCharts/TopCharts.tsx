d3f43419cc8f5c02fab9c58e601c5189ac948c56
sopoturazashvili <sopoturazashvili@gmail.com>
2024-07-24 15:35:10 +0400

    refactor/ topCharts page refactor

---
+++ app/Components/TopCharts/TopCharts.tsx
@@ -0,0 +1,62 @@
+import ChartAlbum from '../ChartAlbum/ChartAlbum';
+import styles from './TopCharts.module.scss'
+
+const TopCharts = () => {
+    const charts = [
+        {
+            id: 1,
+            image: '/images/topHits2024.png',
+            title: 'Top Hits 2024'
+        },
+        {
+            id: 2,
+            image: '/images/chartHitsStars.png',
+            title: 'Chart Hits Stars'
+        },
+        {
+            id: 3,
+            image: '/images/topMusicCharts.png',
+            title: 'Top Music Charts'
+        },
+        {
+            id: 4,
+            image: '/images/superChartHits.png',
+            title: 'Super Chart Hits'
+        },
+        {
+            id: 5,
+            image: '/images/topHits2024.png',
+            title: 'Top Hits 2024'
+        },
+        {
+            id: 6,
+            image: '/images/topHits2024.png',
+            title: 'Top Hits 2024'
+        },
+        {
+            id: 7,
+            image: '/images/topHits2024.png',
+            title: 'Top Hits 2024'
+        },
+        {
+            id: 8,
+            image: '/images/topHits2024.png',
+            title: 'Top Hits 2024'
+        }
+    ]
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>
+                <span className={styles.title}>Top Charts</span>
+            </div>
+            <div className={styles.containerInside}>
+                {charts.map((chart) => (
+                    <ChartAlbum id={chart.id} image={chart.image} title={chart.title} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default TopCharts;
