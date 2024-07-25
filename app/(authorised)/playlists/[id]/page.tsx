538d902d1fefec017c85fcc3f13a9fa26a2bcfe2
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-07-25 19:42:02 +0400

    feat my everiday

--- app/(authorised)/playlists/[id]/page.tsx
+++ app/(authorised)/playlists/[id]/page.tsx
@@ -1,7 +1,27 @@
-const playlist = ()=> {
-    return(
-        <div>PLAYLIST</div>
-    )
-}
+"use client";
 
-export default playlist
+import TopFourHits from "@/app/Components/TopFourHits/TopFourHits";
+import styles from "./page.module.scss";
+
+const playlist = () => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <span className={styles.pageName}>nwioenvien</span>
+      </div>
+      <div className={styles.photoAndNameCont}>
+        <div className={styles.photoAndName}>
+          <img src="/images/playlistPhoto.svg" />
+          <span className={styles.name}>My Everyday</span>
+        </div>
+      </div>
+      <div className={styles.topFourHits}>
+        <TopFourHits />
+        <TopFourHits />
+        <TopFourHits />
+      </div>
+    </div>
+  );
+};
+
+export default playlist;
