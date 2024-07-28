7eb51293843f452b23985dd432660af029f9824f
kuchadze <158141771+kuchadze@users.noreply.github.com>
2024-07-28 19:15:27 +0400

    change input range styles

--- app/Components/PlayerControler/TabletPlayer/TabletVolume/TabletVolumeInput/TabletVolumeInput.tsx
+++ app/Components/PlayerControler/TabletPlayer/TabletVolume/TabletVolumeInput/TabletVolumeInput.tsx
@@ -1,3 +1,51 @@
+// import { useRecoilState } from "recoil";
+// import styles from "./VolumeInput.module.scss";
+// import { isPlayingState, mutedState, volumeState } from "@/app/state";
+
+// interface Props {
+//   TabletaudioRef: React.MutableRefObject<HTMLAudioElement | null>;
+//   tabletWidth: number;
+// }
+
+// const TabletVolumeInput = (props: Props) => {
+//   const [volume, setVolume] = useRecoilState(volumeState);
+//   const [muted, setMuted] = useRecoilState(mutedState);
+//   const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+//   const TabletVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const newVolume = parseFloat(e.target.value);
+//     setVolume(newVolume);
+//     if (props.TabletaudioRef.current) {
+//       props.TabletaudioRef.current.volume = newVolume / 100;
+//     }
+//   };
+
+//   const tabletinputVolume = {
+//     height: "var(--borderRadius, 4px)",
+//     width: `${props.tabletWidth}px`,
+//   };
+//   return (
+//     <>
+//       <input
+//         style={tabletinputVolume}
+//         type="range"
+//         min={0}
+//         max={100}
+//         value={muted ? 0 : volume}
+//         onChange={TabletVolumeChange}
+//         onPlay={
+//           isPlaying
+//             ? props.TabletaudioRef?.current?.play()
+//             : props.TabletaudioRef?.current?.pause()
+//         }
+//       />
+//     </>
+//   );
+// };
+
+// export default TabletVolumeInput;
+
+
 import { useRecoilState } from "recoil";
 import styles from "./VolumeInput.module.scss";
 import { isPlayingState, mutedState, volumeState } from "@/app/state";
@@ -10,7 +58,7 @@ interface Props {
 const TabletVolumeInput = (props: Props) => {
   const [volume, setVolume] = useRecoilState(volumeState);
   const [muted, setMuted] = useRecoilState(mutedState);
-  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [isPlaying] = useRecoilState(isPlayingState);
 
   const TabletVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newVolume = parseFloat(e.target.value);
@@ -20,27 +68,25 @@ const TabletVolumeInput = (props: Props) => {
     }
   };
 
-  const tabletinputVolume = {
+  const progressPercent = volume;
+  const trackStyle = {
+    background: `linear-gradient(to right, #5E4BE2 ${progressPercent}%, #292929 ${progressPercent}%)`,
     height: "var(--borderRadius, 4px)",
     width: `${props.tabletWidth}px`,
   };
+
   return (
-    <>
-      <input
-        style={tabletinputVolume}
-        type="range"
-        min={0}
-        max={100}
-        value={muted ? 0 : volume}
-        onChange={TabletVolumeChange}
-        onPlay={
-          isPlaying
-            ? props.TabletaudioRef?.current?.play()
-            : props.TabletaudioRef?.current?.pause()
-        }
-      />
-    </>
+    <input
+      type="range"
+      min={0}
+      max={100}
+      value={muted ? 0 : volume}
+      onChange={TabletVolumeChange}
+      className={styles.volumeRange}
+      style={trackStyle}
+    />
   );
 };
 
 export default TabletVolumeInput;
+
