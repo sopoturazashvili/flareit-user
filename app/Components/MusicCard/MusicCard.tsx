3d1bbd878ccbd39bf412273bfe8754dd6039f3dd
sopoturazashvili <sopoturazashvili@gmail.com>
2024-08-16 12:48:50 +0400

    created dropdownmenu/ needs fix

--- app/Components/MusicCard/MusicCard.tsx
+++ app/Components/MusicCard/MusicCard.tsx
@@ -1,6 +1,9 @@
 import styles from './MusicCard.module.scss';
 import LikeButton from '../LikeButton/LikeButton';
 import DeleteBox from '../DleleteBox/DeleteBox';
+import DropDownMenu from '../DropDownMenu/DropDownMenu';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
+import { useEffect, useRef, useState } from 'react';
 
 interface Props {
     image: string;
@@ -14,6 +17,37 @@ interface Props {
 }
 
 const MusicCard = (props: Props) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [menuPositionClasses, setMenuPositionClasses] = useState<string[]>(
+        [],
+    );
+    const musicCardRef = useRef<HTMLDivElement>(null);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    useEffect(() => {
+        if (menuOpen && musicCardRef.current) {
+            const rect = musicCardRef.current.getBoundingClientRect();
+
+            console.log('this is rect left', rect.left);
+            console.log('this is rect right', rect.right);
+
+            if (rect.left > rect.right) {
+                setMenuPositionClasses((prevClasses) => [
+                    ...prevClasses,
+                    'styles.left',
+                ]);
+            } else {
+                setMenuPositionClasses((prevClasses) => [
+                    ...prevClasses,
+                    'styles.right',
+                ]);
+            }
+        }
+    }, [menuOpen]);
+
     return (
         <div className={styles.musicCard}>
             <div className={styles.musicCardHeader} onClick={props.onClick}>
@@ -34,12 +68,22 @@ const MusicCard = (props: Props) => {
                     </span>
                 </div>
             </div>
-            <div>
+            <div className={styles.buttonsContainer}>
                 {props.deleteOrLike ? (
                     <DeleteBox id={props.id} />
                 ) : (
                     <LikeButton isLiked={false} id={props.id} />
                 )}
+                <div onClick={toggleMenu} className={styles.menu}>
+                    <BiDotsVerticalRounded size={24} color="white" />
+                    {menuOpen && (
+                        <div
+                            className={`${styles.dropDownMenu} ${menuPositionClasses.join(' ').trim()}`}
+                        >
+                            <DropDownMenu />
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
