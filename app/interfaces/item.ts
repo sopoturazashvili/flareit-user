034aaf20132dbc0ab4923a19e50e44008a097db5
sopoturazashvili <sopoturazashvili@gmail.com>
2024-08-30 16:28:58 +0400

    refactor/register page

--- app/interfaces/item.ts
+++ app/interfaces/item.ts
@@ -3,3 +3,22 @@ export interface Item {
     backgroundImage: string;
     title: string;
 }
+
+export interface RegisterInputs {
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+export interface ErrorResponse {
+    message: string;
+}
+
+export interface AuthInputs {
+    email: string;
+    password: string;
+}
+
+export interface Response {
+    access_token: string;
+}
