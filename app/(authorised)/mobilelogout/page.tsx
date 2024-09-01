c77fca8e17040e228e15b2dc0b8d3953221adb7d
luka <luka.kuchadze25@gmail.com>
2024-09-01 19:30:36 +0400

    refactor mobiles pages

--- app/(authorised)/mobilelogout/page.tsx
+++ app/(authorised)/mobilelogout/page.tsx
@@ -1,36 +1,7 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
-import styles from './page.module.scss';
+import MobileLogOut from '@/app/Components/MobileLogOut/MobileLogOut';
 
 const mobileLogOut = () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const router = useRouter();
-
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        router.push('/auth');
-    };
-    return (
-        <div className={styles.mobileLogOut}>
-            <div className={styles.profileAndButton}>
-                <div className={styles.profileContainer}>
-                    <div className={styles.profileAndSearch}>
-                        <div className={styles.profileBox}>
-                            <p className={styles.profile}>Profile</p>
-                        </div>
-                        <div className={styles.mobileLogOutName}>
-                            <img src="/Image/LogOut.svg" alt="LogOut" />
-                            <p className={styles.color}></p>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.button} onClick={handleLogout}>
-                    <button className={styles.logOutButton}>Log out</button>
-                </div>
-            </div>
-        </div>
-    );
+    return <MobileLogOut />;
 };
 
 export default mobileLogOut;
---
+++ app/Components/MobileLogOut/MobileLogOut.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import styles from './MobileLogOut.module.scss';
+
+const MobileLogOut = () => {
+    const router = useRouter();
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        router.push('/auth');
+    };
+    return (
+        <div className={styles.mobileLogOut}>
+            <div className={styles.profileAndButton}>
+                <div className={styles.profileContainer}>
+                    <div className={styles.profileAndSearch}>
+                        <div className={styles.profileBox}>
+                            <p className={styles.profile}>Profile</p>
+                        </div>
+                        <div className={styles.mobileLogOutName}>
+                            <img src="/Image/LogOut.svg" alt="LogOut" />
+                            <p className={styles.color}></p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.button} onClick={handleLogout}>
+                    <button className={styles.logOutButton}>Log out</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MobileLogOut;
