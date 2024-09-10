ca6f8f25425b709ac65c385ac84362db451356ec
luka <luka.kuchadze25@gmail.com>
2024-09-10 11:33:20 +0400

    create shuffle

--- app/Components/PlayerControler/DesktopMusicSwitch/LeftSwitch/LeftSwitch.tsx
+++ app/Components/PlayerControler/DesktopMusicSwitch/LeftSwitch/LeftSwitch.tsx
@@ -1,13 +1,23 @@
 import { useRecoilState } from 'recoil';
-import { indexState } from '@/app/state';
+import { indexState, musicGlobalState } from '@/app/state';
 import styles from './LeftSwitch.module.scss';
+import { shuffleState } from '@/app/Components/state';
 
 const LeftSwitch = () => {
     const [index, setIndex] = useRecoilState(indexState);
+    const [musicSrc] = useRecoilState(musicGlobalState);
+    const [isShuffle] = useRecoilState(shuffleState);
 
     const playPrevious = () => {
-        if (index > 0) {
-            setIndex(index - 1);
+        if (isShuffle) {
+            // Если шаффл включен, выбираем случайный трек
+            const randomIndex = Math.floor(Math.random() * musicSrc.length);
+            setIndex(randomIndex);
+        } else {
+            // Если шаффл выключен, выбираем предыдущий трек по порядку
+            setIndex((prevIndex) =>
+                prevIndex > 0 ? prevIndex - 1 : musicSrc.length - 1,
+            );
         }
     };
 
@@ -16,9 +26,7 @@ const LeftSwitch = () => {
             className={styles.LeftSwitch}
             src="/PlayerControler/LeftSwitch.svg"
             alt="Left Switch"
-            onClick={() => {
-                playPrevious();
-            }}
+            onClick={playPrevious}
         />
     );
 };
--- app/Components/PlayerControler/Shuffle/Shufle.tsx
+++ app/Components/PlayerControler/Shuffle/Shufle.tsx
@@ -1,11 +1,29 @@
+import { useRecoilState } from 'recoil';
 import styles from './Shuffle.module.scss';
+import { shuffleState } from '../../state';
+import { indexState, musicGlobalState } from '@/app/state';
+
 const Shuffle = () => {
+    const [isShuffle, setIsShuffle] = useRecoilState(shuffleState);
+    const [index, setIndex] = useRecoilState(indexState);
+    const [musicSrc] = useRecoilState(musicGlobalState);
+
+    const handleShuffleClick = () => {
+        setIsShuffle(!isShuffle);
+    };
+
     return (
-        <img
-            className={styles.shuffle}
-            src="/PlayerControler/Messy.svg"
-            alt="Messy"
-        />
+        <div onClick={handleShuffleClick}>
+            {isShuffle ? (
+                <img src="/PlayerControler/pinkShuffle.svg" alt="shuffle" />
+            ) : (
+                <img
+                    className={styles.shuffle}
+                    src="/PlayerControler/shuffle.svg"
+                    alt="Messy"
+                />
+            )}
+        </div>
     );
 };
 
